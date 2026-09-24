accb4be53e01103929572839c07312cceedaa0b6
Amber "Dr. Ro" Rodenbo <awrodenbo@gmail.com>
2026-09-24 12:16:41 -0400

    Provide appearance theme app-wide

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from '../src/theme/context';
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
+    <ThemeProvider>
+      <SafeAreaProvider>
       <StatusBar style="dark" />
       <Stack screenOptions={{ headerShown: false, animation: 'fade' }} />
     </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
